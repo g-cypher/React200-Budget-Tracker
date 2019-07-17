c77f0921767d8e77618343cf43df004b213cfd11
Garrett <cyph17@gmail.com>
2019-07-17 15:11:25 -0700

    app in working state, making commit

---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,13 @@
+
+const defaultState = {
+    description: '',
+    amount: '',
+    lineItems: []
+  };
+  
+  export default function IncomeReducer(state = defaultState, action) {
+    // the `state = defaultState` above is new ES6 syntax
+    // for defining a default value on a parameter
+    return state;
+  }
+  
